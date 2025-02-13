c4705d49157630e8611853ab34106fbfbb3e4579
Eric Weston (Desktop) <ericweston610@gmail.com>
2025-02-12 23:05:49 -0700

    Finished

--- Fractional/RationalNumber.cs
+++ Fractional/RationalNumber.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Fractional
 {
-    public class RationalNumber
+    public class RationalNumber : IEquatable<RationalNumber>, IComparable<RationalNumber>
     {
         int numerator;
         int denominator;
@@ -19,6 +19,27 @@ namespace Fractional
             var gcd = GreatestCommonDenominator(numerator, denominator);
             numerator /= gcd;
             denominator /= gcd;
+        }
+        public bool Equals(RationalNumber? other)
+        {
+            //throw new NotImplementedException();
+            if (other == null)
+            {
+                return false;
+            }
+            return this.numerator == other.numerator && this.denominator == other.denominator;
+        }
+
+        public int CompareTo(RationalNumber? other)
+        {
+            //throw new NotImplementedException();
+            if (other == null)
+            {
+                return 1;
+            }
+            int left = this.numerator * other.denominator;
+            int right = other.numerator * this.denominator;
+            return left.CompareTo(right);
         }
 
         public override string ToString()
